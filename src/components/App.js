c340a259ae37d9b86c500a5a780574400d50f542
Doug Bostick <doug.bostick@fullstackacademy.com>
2022-08-24 11:23:33 -0400

    complete auth code flow

--- src/components/App.js
+++ src/components/App.js
@@ -2,7 +2,12 @@ import React from 'react';
 import Signup from './Signup'
 import { Container } from 'react-bootstrap';
 import { AuthProvider } from '../contexts/AuthContext';
-import { BrowserRouter as Router, Routes, Route} from 'react-router-dom'
+import { BrowserRouter as Router, Routes, Route, Naviagte} from 'react-router-dom'
+import Dashboard from './Dashboard'
+import Login from './Login'
+import PrivateRoute from './PrivateRoute';
+import ForgotPassword from './ForgotPassword';
+import UpdateProfile from './UpdateProfile';
 
 function App() {
   
@@ -16,7 +21,13 @@ function App() {
          <Router>
          <AuthProvider> 
           <Routes>
-            <Route path='/signup' element={Signup} />
+            {/* <PrivateRoute exact path='/' element={<Dashboard />}/> */}
+            <Route path='/update-profile' element={<PrivateRoute><UpdateProfile/></PrivateRoute>}/>
+            <Route path='/' element={<PrivateRoute> <Dashboard/></PrivateRoute>} />
+            <Route path='/signup' element={<Signup />} />
+            <Route path='/login' element={<Login />} />
+            <Route path='/forgot-password' element={<ForgotPassword />} />
+
           </Routes>
          </AuthProvider>
          </Router>
--- src/components/Login.js
+++ src/components/Login.js
@@ -1,13 +1,12 @@
 import React, { useRef, useState } from 'react'
 import { Card, Form, Button, Alert } from 'react-bootstrap'
 import { useAuth } from '../contexts/AuthContext'
-import { Link, useNavigate } from 'react-router-dom' 
+import { Link, useNavigate } from 'react-router-dom'
 
-export default function Signup() {
+export default function Login() {
     const emailRef = useRef();
     const passwordRef = useRef();
-    const passwordConfirmRef = useRef(); 
-    const { signup } = useAuth();
+    const { login } = useAuth();
     const [error, setError] = useState('');
     const [loading, setLoading] = useState(false);
     const navigate = useNavigate();
@@ -15,17 +14,13 @@ export default function Signup() {
     async function handleSubmit(e) {
       e.preventDefault()
   
-      if (passwordRef.current.value !== passwordConfirmRef.current.value) {
-        return setError("Passwords do not match")
-      }
-  
       try {
         setError("")
         setLoading(true)
-        await signup(emailRef.current.value, passwordRef.current.value)
-        navigate('/')
+        await login(emailRef.current.value, passwordRef.current.value)
+        navigate('/', { replace: true })
         } catch {
-            setError('Failed to create and account')
+            setError('Failed to sign in')
         }
         setLoading(false)
     }
@@ -34,7 +29,7 @@ export default function Signup() {
     <>
     <Card>
         <Card.Body>
-            <h2 className='text-center mb-4'>Sign Up</h2>
+            <h2 className='text-center mb-4'>Login</h2>
             {error && <Alert variant='danger'>{error}</Alert>}
             <Form onSubmit={handleSubmit}>
             <Form.Group id="email">
@@ -45,19 +40,19 @@ export default function Signup() {
               <Form.Label>Password</Form.Label>
               <Form.Control type="password" ref={passwordRef} required />
             </Form.Group>
-            <Form.Group id="password-confirm">
-              <Form.Label>Password Confirmation</Form.Label>
-              <Form.Control type="password" ref={passwordConfirmRef} required />
-            </Form.Group>
             <Button disabled={loading} className="w-100 mt-3" type="submit">
-              Sign Up
+              Login
             </Button>
           </Form>
+          <div className='w-100 text-center mt-3'>
+            <Link to='/forgot-password'>Forgot password?</Link>
+          </div>
         </Card.Body>
     </Card>
     <div className='w-100 text-center mt-2'>
-        Already have an account? <Link to='/login'>Login</Link>
+        Need an account? <Link to='/signup'>Sign Up</Link>
     </div>
     </>
   )
 }
+
